8fe444bc3623cc1c325ed6416f44baf52025d0c0
Zdenek Sejcek <zdenek@sejcek.cz>
2014-10-17 16:22:48 +1300

    added playback

--- BabyWhisperer/FirstViewController.swift
+++ BabyWhisperer/FirstViewController.swift
@@ -11,9 +11,26 @@ import AVFoundation;
 
 class FirstViewController: UIViewController {
     var recorder : AVAudioRecorder;
+    var audioPlayer : AVAudioPlayer;
     var timer: NSTimer = NSTimer();
     
+    @IBOutlet weak var playStopButton: UIButton!
     @IBOutlet weak var decibelsLabel: UILabel!
+    
+    @IBAction func buttonTouchedDown(sender: UIButton) {
+        if (audioPlayer.playing) {
+            audioPlayer.stop()
+            sender.setTitle("Start playing", forState: UIControlState.Normal)
+//            playStopButton.setTitle("Start playing", forState: UIControlState.Normal)
+        } else {
+            audioPlayer.currentTime = 0;
+            audioPlayer.prepareToPlay();
+            audioPlayer.play()
+
+            sender.setTitle("Stop playing", forState: UIControlState.Normal)
+//            playStopButton.setTitle("Stop playing", forState: UIControlState.Normal)
+        }
+    }
     
     required init(coder aDecoder: NSCoder) {
         AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
@@ -32,31 +49,27 @@ class FirstViewController: UIViewController {
 
         self.recorder.record()
 
+        var mp3Path = NSBundle.mainBundle().pathForResource("lullaby", ofType: "mp3")
+        var mp3Url = NSURL.URLWithString(mp3Path!);
+        
+        audioPlayer = AVAudioPlayer(contentsOfURL: mp3Url, error: nil)
+        
         super.init(coder: aDecoder)
-        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "test", userInfo: nil, repeats: true)
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "updateMeters", userInfo: nil, repeats: true)
     }
     
-    func test() {
+    func updateMeters() {
         recorder.updateMeters();
         decibelsLabel.text = NSString(format:"%.2f", recorder.peakPowerForChannel(0));
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-/*        var refreshAlert = UIAlertView()
-        refreshAlert.title = "Refresh?"
-        refreshAlert.message = "All data will be lost."
-        refreshAlert.addButtonWithTitle("Cancel")
-        refreshAlert.addButtonWithTitle("OK")
-        refreshAlert.show()
-*/
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
